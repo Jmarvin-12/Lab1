f9c54af269f8bb8bfd2a37f94f8e1389270e63a0
Marvin <josehenriquez.niu@gmail.com>
2021-06-10 09:39:46 -0600

    Laboratorio 1

---
+++ Lab1/Controllers/HomeController.cs
@@ -0,0 +1,129 @@
+﻿using Lab1.Logic;
+using Lab1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public DateTime dateTime = new DateTime();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+
+            ViewBag.fecha = DateTime.Now;
+
+            return View();
+        }
+
+        public IActionResult TarjetaPresentacion()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Operations()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Operations( string num1, string num2, string? num3)
+        {
+            
+            Operations op = new Operations();
+
+            try
+            {
+                double n1 = Convert.ToDouble(num1);
+                double n2 = Convert.ToDouble(num2);
+                double n3 = Convert.ToDouble(num3);
+
+                op.Operaciones(n1, n2, n3);
+
+                ViewBag.operationsList = op.OperationsList();
+
+            }
+            catch (DivideByZeroException ex)
+            {
+                ViewBag.OpError = "No se puede dividir un numero entre 0, ingrese otros valores. " + ex.Message;
+            }
+            catch (ArithmeticException ex)
+            {
+                ViewBag.OpError = "Datos ingresados son nulos, ingrese valores validos. " + ex.Message;
+            }
+            catch(FormatException ex)
+            {
+                ViewBag.OpError = "Dato ingresado es un string, ingrese un dato numerico. " + ex.Message;
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreateVehicle()
+        {
+
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateVehicle(string marca, string year, string color, string llantas, bool categoria)
+        {
+            Operations op = new Operations();
+            Vehicle vehicle = new Vehicle();
+
+            try
+            {
+                int anio = Convert.ToInt32(year);
+                int tires = Convert.ToInt32(llantas);
+
+               op.CreateVehicle(marca, anio, color, tires, categoria);
+
+                ViewBag.Vehicles = op.VehiclesList();
+
+            }
+            catch(NullReferenceException ex)
+            {
+                ViewBag.VehinullError = "Ingrese todos los datos "+ ex.Message;
+            }
+            catch (FormatException ex)
+            {
+                ViewBag.Vehistr = "Debe de ingresar solamente numeros, error causado: " +ex.Message;
+            }
+            catch (ArithmeticException ex)
+            {
+                ViewBag.VehiError = ex.Message;
+            }
+
+
+            return View(vehicle);
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Lab1/Logic/Operations.cs
@@ -0,0 +1,133 @@
+﻿using Lab1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab1.Logic
+{
+    public class Operations
+    {
+        public static List<double> operationsList { get; set; }
+
+        public static List<Vehicle> vehicleList = new List<Vehicle>();
+
+        public List<double> OperationsList()
+        {
+
+            return operationsList;
+        }
+
+        public List<Vehicle> VehiclesList()
+        {
+            return vehicleList;
+        }
+
+        public List<double> Operaciones(double n1, double n2, double? n3)
+        {
+
+            operationsList = new List<double>();
+
+            try
+            {
+                double suma = n1 + n2;
+                operationsList.Add(suma);
+                double resta = n1 - n2;
+                operationsList.Add(resta);
+                double multi = n1 * n2;
+                operationsList.Add(multi);
+
+                if (n1 == 0 && n2 == 0 && n3 ==0)
+                {
+
+                    throw new ArithmeticException();
+
+                }else if (n2 == 0)
+                {
+                    throw new DivideByZeroException();
+                }
+                else
+                {
+                    double divi = n1 / n2;
+                    operationsList.Add(divi);
+                }
+
+
+                double potencia = Math.Pow(suma, (double)n3);
+                operationsList.Add(potencia);
+                double radica = Math.Sqrt(suma);
+                operationsList.Add(radica);
+
+            }
+            catch (FormatException)
+            {
+                throw;
+            }
+                
+
+            return operationsList;
+        }
+
+        public List<Vehicle> CreateVehicle(string Marca, int year, string color, int llantas, bool categoria)
+        {
+            
+            try
+            {
+                string category;
+                if(categoria)
+                {
+                    category = "Sedan";
+                }
+                else
+                {
+                    category = "Pickup";
+                }
+
+                vehicleList.Add(new Vehicle(Marca, year, color, llantas, category));
+            }
+            catch (NullReferenceException)
+            {
+                throw;
+            }
+            catch (ArgumentNullException)
+            {
+                throw new ArgumentNullException();
+            }
+            catch (ArithmeticException) {
+                throw new ArithmeticException();
+            }
+
+
+            return vehicleList;
+        }
+
+    }
+
+    public class Vehicle
+    {
+        public string Id { get; set; }
+        public string Marca { get; set; }
+        public int year { get; set; }
+        public string color { get; set; }
+        public int llantas { get; set; }
+        public string category { get; set; }
+        public DateTime EntryDate { get; set; }
+
+        public Vehicle()
+        {
+        }
+
+        public Vehicle(string Marca, int year, string color, int llantas, string category)
+        {
+            this.Id = Guid.NewGuid().ToString();
+            this.Marca = Marca;
+            this.year = (int)year;
+            this.color = color;
+            this.llantas = llantas;
+            this.category = category;
+            this.EntryDate = DateTime.Now;
+        }
+
+
+    }
+}
